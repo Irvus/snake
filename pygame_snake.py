145969c31b0ed37588966ef00098f5dc53987d58
irvus <iatarasenko@edu.hse.ru>
2021-05-02 14:08:23 +0300

    конфиг, комменты

--- pygame_snake.py
+++ pygame_snake.py
@@ -1,24 +1,13 @@
-# Pygame шаблон - скелет для нового проекта Pygame
 import pygame
 import random
+from config import WIDTH, HEIGHT, FPS, WHITE, BLACK, RED, GREEN, BLUE
 
-WIDTH = 360
-HEIGHT = 480
-FPS = 30
 
-# Задаем цвета
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-RED = (255, 0, 0)
-GREEN = (0, 255, 0)
-BLUE = (0, 0, 255)
-
-# Создаем игру и окно
-pygame.init()
-pygame.mixer.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.init()  # старт pygame
+pygame.mixer.init()  # тут что-то со звуком
+screen = pygame.display.set_mode((WIDTH, HEIGHT))  # создание окна
 pygame.display.set_caption("My Game")
-clock = pygame.time.Clock()
+clock = pygame.time.Clock()  # эт для фпс, но, вроде бы, это просто часы
 
 # Цикл игры
 running = True
@@ -27,8 +16,7 @@ while running:
     clock.tick(FPS)
     # Ввод процесса (события)
     for event in pygame.event.get():
-        # check for closing window
-        if event.type == pygame.QUIT:
+        if event.type == pygame.QUIT:  # check for closing window
             running = False
 
     # Обновление
@@ -36,6 +24,6 @@ while running:
     # Рендеринг
     screen.fill(BLACK)
     # После отрисовки всего, переворачиваем экран
-    pygame.display.flip()
+    pygame.display.flip()  # Для двойной буферизации, что бы это ни было
 
 pygame.quit()
